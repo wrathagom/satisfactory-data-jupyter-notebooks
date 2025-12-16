ad57074fb4934ca539529a118655a5816afb5d3d
Wrathagom <calebmkeller@gmail.com>
2025-12-15 22:10:36 -0500

    Checking some new notebooks in.

--- data/notebooks/functions/custom.py
+++ data/notebooks/functions/custom.py
@@ -5,3 +5,19 @@ def suffix(d):
 
 def custom_strftime(format, t):
     return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
+  
+def format_number(num):
+    """Format numbers in a human-friendly way"""
+    if num >= 1_000_000:
+        return f'{num/1_000_000:.1f}M'
+    elif num >= 1_000:
+        return f'{num/1_000:.1f}K'
+    else:
+        return f'{num:.0f}'
+      
+def list_endpoints():
+    endpoints = ['getAssembler', 'getBelts', 'getCables', 'getExtractor', 'getFoundry',
+    'getGenerators', 'getHypertube', 'getLifts', 'getManufacturer', 'getStorageInv', 'getConstructor',
+    'getStorageInv', 'getPackager', 'getPipes', 'getPower', 'getRefinery', 'getSmelter', 'getSplitterMerger', 'getTrainRails']
+
+    return endpoints
